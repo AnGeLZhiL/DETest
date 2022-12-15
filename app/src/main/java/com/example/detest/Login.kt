87184d7ebaa02075b7b2d44631504b034162be52
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2022-12-16 00:45:31 +0300

    theylf

--- app/src/main/java/com/example/detest/Login.kt
+++ app/src/main/java/com/example/detest/Login.kt
@@ -1,11 +1,20 @@
 package com.example.detest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.detest.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signInOnClick.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/detest/SplashScreen.kt
+++ app/src/main/java/com/example/detest/SplashScreen.kt
@@ -1,11 +1,22 @@
 package com.example.detest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.detest.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, Login::class.java))
+            finish()
+        }, 2000)
     }
 }
